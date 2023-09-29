442efe1911eb38ff8db46f0d8754e5c074a15cd9
Gabriel Bento <gab.silva.bento@gmail.com>
2023-09-29 10:57:54 -0300

    Feat: add config mapper

---
+++ src/main/java/pdev/com/agenda/config/ModalMapperConfig.java
@@ -0,0 +1,13 @@
+package pdev.com.agenda.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModalMapperConfig {
+    @Bean
+    public ModelMapper modalMapper(){
+        return new ModelMapper();
+    }
+}
