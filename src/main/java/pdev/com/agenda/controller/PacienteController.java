3ae9f8ba3d1174196bd05c37b53468c6c2143c44
Gabriel Bento <gab.silva.bento@gmail.com>
2023-10-03 10:14:21 -0300

    Feat: Add documentacao da rota de buscar todos os pacientes

--- src/main/java/pdev/com/agenda/controller/PacienteController.java
+++ src/main/java/pdev/com/agenda/controller/PacienteController.java
@@ -1,5 +1,11 @@
 package pdev.com.agenda.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +22,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("paciente")
+@Tag(name = "Paciente", description = "Rotas para o Paciente")
 public class PacienteController {
 
 
@@ -33,6 +40,14 @@ public class PacienteController {
         return ResponseEntity.status(HttpStatus.CREATED).body(pacienteResponse);
     }
 
+    @Operation(
+            summary = "Lista todos os Pacientes",
+            description = "Recupera todos os Pacientes e suas informações",
+            tags = { "Paciente", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Paciente.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping
     public ResponseEntity<List<PacienteResponse>> listar(){
         List<Paciente> pacientes = pacienteService.buscarTodos();
