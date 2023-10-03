0f3ca5685dd24f391d02134e2705e5dbcb1fd746
Gabriel Bento <gab.silva.bento@gmail.com>
2023-10-03 15:07:02 -0300

    Feat: Add configuracao do spring security

---
+++ src/main/java/pdev/com/agenda/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package pdev.com.agenda.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(
+                (authorize) -> {
+                    authorize.requestMatchers("/public").permitAll();
+                    authorize.requestMatchers("/logout").permitAll();
+                    authorize.requestMatchers("/paciente").permitAll();
+                    authorize.anyRequest().authenticated();
+                })
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
